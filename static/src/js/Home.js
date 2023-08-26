2922806d2f672615a41cd01508e70823ff56d98f
Paddy <paddybalinia@gmail.com>
2023-08-25 22:58:38 -0300

    feat: actualiza header js

--- static/src/js/Home.js
+++ static/src/js/Home.js
@@ -1,9 +1,74 @@
 (function () {
   "use strict";
 
+  var Header = document.querySelector(".header"),
+    lastScrollY =
+      window.scrollY ||
+      window.pageYOffset ||
+      document.documentElement.scrollTop,
+    startSticky =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight,
+    ticking = false;
   // Constructor
-  function Constructor() {}
+  function Constructor() {
+    window.addEventListener("scroll", onScroll);
+  }
 
+  /**
+   * Evento onScroll
+   * @return void
+   */
+  function onScroll() {
+    lastScrollY =
+      window.scrollY ||
+      window.pageYOffset ||
+      document.documentElement.scrollTop;
+
+    requestTick();
+  }
+  /**
+   * Validamos que se haya ejecutado correctamente el onScroll() antes de pintar
+   * un nuevo frame.
+   * @return void
+   */
+  function requestTick() {
+    if (!ticking) {
+      requestAnimationFrame(update);
+      ticking = true;
+    }
+  }
+  /**
+   *
+   *
+   * @return void
+   */
+  function update() {
+    const menuLinks = document.querySelectorAll(".header__ul a");
+
+    Header.classList[lastScrollY >= startSticky - 50 ? "add" : "remove"](
+      "header--beforesticky"
+    );
+    Header.classList[lastScrollY >= startSticky ? "add" : "remove"](
+      "header--sticky"
+    );
+
+    menuLinks.forEach((link) => {
+      const section = document.querySelector(link.getAttribute("href"));
+      const bounding = section.getBoundingClientRect();
+
+      if (
+        bounding.top <= window.innerHeight / 2 &&
+        bounding.bottom >= window.innerHeight / 2
+      ) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+    ticking = false;
+  }
   // Export
   window.Header = Constructor();
 })();
