ffa9e83c4467185a7737e4e338c10a1c1e759e44
Paddy <paddybalinia@gmail.com>
2024-01-09 10:09:04 -0300

    feat: add paralax

--- static/src/js/Home.js
+++ static/src/js/Home.js
@@ -255,6 +255,44 @@
   });
 })(window, document);
 
+(function () {
+  "use strict";
+
+  function moveIt() {
+    var windowElement = window;
+    var instances = [];
+
+    document
+      .querySelectorAll("[data-scroll-speed]")
+      .forEach(function (element) {
+        instances.push(new MoveItItem(element));
+      });
+
+    windowElement.onscroll = function () {
+      var scrollTop =
+        windowElement.scrollY || document.documentElement.scrollTop;
+      instances.forEach(function (inst) {
+        inst.update(scrollTop);
+      });
+    };
+  }
+
+  var MoveItItem = function (element) {
+    this.element = element;
+    this.speed = parseInt(this.element.getAttribute("data-scroll-speed"));
+  };
+
+  MoveItItem.prototype.update = function (scrollTop) {
+    var pos = scrollTop / this.speed;
+    this.element.style.transform = "translateY(" + -pos + "px)";
+    // Add fade-out effect
+    var opacity = 1 - Math.max(0, pos / 100);
+    this.element.style.opacity = opacity;
+  };
+
+  moveIt();
+})();
+
 ;(function () {
   "use strict"
   const textos = document.querySelectorAll(".texto-animado")
